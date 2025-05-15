d95f816bd85835768184ef5f70d78ff761528f07
Matonice <omotoso.abdulmatin4@gmail.com>
2025-05-15 17:29:58 +0100

    edit the interface to not ask for the openai api key

--- QA_Chatbot/app.py
+++ QA_Chatbot/app.py
@@ -22,8 +22,8 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-def generate_response(question, api_key, llm, temperature, max_token):
-    openai.api_key=api_key
+def generate_response(question, llm, temperature, max_token):
+    #openai.api_key=api_key
     llm=ChatOpenAI(model=llm, temperature=temperature, max_tokens=max_token)
     output_parser=StrOutputParser()
     chain=prompt|llm|output_parser
@@ -35,7 +35,7 @@ def generate_response(question, api_key, llm, temperature, max_token):
 st.title("QA Chatbot With OpenAI")
 
 st.sidebar.title("Settings")
-api_key = st.sidebar.text_input("Enter your Open AI API KEY:", type="password")
+#api_key = st.sidebar.text_input("Enter your Open AI API KEY:", type="password")
 
 llm = st.sidebar.selectbox("Select an Open AI Model", ["gpt-4o","gpt-4-turbo","gpt-4"])
 
@@ -46,7 +46,7 @@ st.write("Go ahead and ask any question")
 user_input = st.text_input("You:")
 
 if user_input:
-    response = generate_response(user_input, api_key, llm, temperature, max_tokens)
+    response = generate_response(user_input, llm, temperature, max_tokens)
     st.write(response)
 else:
     st.write("Please provide the query")
